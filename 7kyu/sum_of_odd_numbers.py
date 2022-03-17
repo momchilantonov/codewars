56c6566b75ef433f8532acc365e6d8422615bab2
momchilantonov <eng.antonov@gmail.com>
2022-03-18 01:10:47 +0200

    Sum of a odd numbers

---
+++ 7kyu/sum_of_odd_numbers.py
@@ -0,0 +1,12 @@
+def row_sum_odd_numbers(n):
+    """
+    Given the triangle of consecutive odd numbers
+    """
+
+    result = 0
+    start = (n * n)-(n - 1)
+    end = start+n * 2
+    for i in range(start, end, 2):
+        result += i
+
+    return result
