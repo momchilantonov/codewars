af4491ba9f56bf2630d4c3e43c663b722f5f31a9
momchilantonov <eng.antonov@gmail.com>
2022-03-21 16:58:10 +0200

    Growth of a population

---
+++ 7kyu/growth_of_a_population.py
@@ -0,0 +1,20 @@
+def nb_year(p0, percent, aug, p):
+    """
+    In a small town the population is p0 = 1000 at the beginning of a year. 
+    The population regularly increases by 2 percent per year and moreover 50
+    new inhabitants per year come to live in the town. How many years does the 
+    town need to see its population greater or equal to p = 1200 inhabitants?
+    More generally given parameters:
+     - p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+     - the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+     - aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+    Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+    """
+
+    years = 0
+
+    while p0 < p:
+        p0 += int(p0 * (percent / 100)+aug)
+        years += 1
+
+    return years
