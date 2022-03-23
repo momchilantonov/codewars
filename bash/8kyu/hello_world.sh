fc0d72d0615edd6f6adbaf014cac88908f5b8018
momchilantonov <eng.antonov@gmail.com>
2022-03-23 02:57:51 +0200

    Hello World

---
+++ bash/8kyu/hello_world.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+################################################################################
+# Make a simple function called greet that returns the most-famous hello world!
+################################################################################
+
+greet() {
+    echo "hello worls!"
+}
+
+# TESTS
+greet
